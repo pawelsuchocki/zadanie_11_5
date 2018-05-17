2db40ae26966d286127bbe93cdc396328af6a0bf
Pawel Suchocki <pawel.suchocki212@gmail.com>
2018-05-17 15:13:59 +0200

    add files

---
+++ script.js
@@ -0,0 +1,19 @@
+var btn1 = new Button('Hello!');
+
+function Button(text) {
+	this.text = text || 'Hello';
+}
+
+Button.prototype = {
+	create: function() {
+		var self = this;
+		this.$element = $('<button>');
+		this.$element.text(this.text);
+		this.$element.click(function() {
+			alert(self.text);
+		});
+		$('body').append(this.$element);
+	}
+}
+
+btn1.create(); // w konsoli pokazuje, że create nie jest funkcja i nie mogę jej wywołać;/
