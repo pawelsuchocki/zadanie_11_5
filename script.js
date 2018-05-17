1c167a25ba3d37b9410d5462929d353b4c5a8e3d
Pawel Suchocki <pawel.suchocki212@gmail.com>
2018-05-17 22:51:57 +0200

    all works

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ function Button(text) {
 Button.prototype = {
 	create: function() {
 		var self = this;
-		var btn1 = new Button('Hello!');
 		this.$element = $('<button>');
 		this.$element.text(this.text);
 		this.$element.click(function() {
@@ -15,4 +14,6 @@ Button.prototype = {
 	}
 }
 
-btn1.create(); // w konsoli pokazuje, że create nie jest funkcja i nie mogę jej wywołać;/
+var btn1 = new Button('Hello!');
+
+btn1.create();
